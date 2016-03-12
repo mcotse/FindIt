589884cada090d3309c18fab51f5a93779b3d217
Michelle Rubinov <michelle.rubinov@genesys.com>
2016-03-12 17:47:00 -0500

    multiple changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+var _ = require('lodash-node');
+
 /**
  * This is modified code from sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
  * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
@@ -81,20 +83,12 @@ function onIntent(intentRequest, session, callback) {
     // console.log("onIntent session.attributes.itemBeingSearched = "+ session.attributes.itemBeingSearched);
 
     // Dispatch to your skill's intent handlers
-    if ("WhereIsItIntent" === intentName) {
-        setItemInSession(intent, session, callback);
+    if ("WhereIsItIntent" === intentName || "DoYouHaveIntent" === intentName) {
+        searchForItem(intent, session, callback);
     } else if ("AnswerIntent" === intentName) {
         setColorInSession(intent, session, callback);
-    } else if ("WhatsMyColorIntent" === intentName) {
-        getColorFromSession(intent, session, callback);
     } else if ("AMAZON.HelpIntent" === intentName) {
         getWelcomeResponse(callback);
-    } else if ("AMAZON.YesIntent" === intentName && session.attributes && session.attributes.itemBeingSearched) {
-        console.log('TEST YES');
-        getLocationOfItem(callback);
-    } else if ("AMAZON.NoIntent" === intentName && session.attributes && session.attributes.itemBeingSearched) {
-        console.log('TEST NO');
-        failedToSetItemInSession(callback);
     } else {
         throw "Invalid intent";
     }
@@ -118,11 +112,10 @@ function getWelcomeResponse(callback) {
     var cardTitle = "Welcome";
     //var speechOutput = "Welcome to the Alexa Skills Kit sample. " +
       //  "Please tell me your favorite color by saying, my favorite color is red";
-    var speechOutput = "Welcome to the Alexa Where Is It sample. How can I help you?";
+    var speechOutput = "Welcome! How can I help you find?";
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
-    var repromptText = "Please tell me what you are looking for by saying, " +
-        "Where can I find apples";
+    var repromptText = "Please tell me what you are looking for by saying, Where can I find apples?";
     var shouldEndSession = false;
 
     callback(sessionAttributes,
@@ -130,10 +123,10 @@ function getWelcomeResponse(callback) {
 }
 
 /**
- * Sets the color in the session and prepares the speech to reply to the user.
+ * Sets the item in the session and prepares the speech to reply to the user.
  */
-function setItemInSession(intent, session, callback) {
-    var cardTitle = intent.name;
+function searchForItem(intent, session, callback) {
+    var intentName = intent.name;
     var itemSearchedFor = intent.slots.Item;
     var repromptText = "";
     var sessionAttributes = {};
@@ -142,43 +135,124 @@ function setItemInSession(intent, session, callback) {
 
     if (itemSearchedFor) {
         sessionAttributes = {itemSearchedFor: itemSearchedFor.value};
-        speechOutput = "Are you looking for " + itemSearchedFor.value + "?";
-        repromptText = "You can ask me where something is by saying, where is the salt?";
+
+        // e.g. "Dairy items like " + itemSearchedFor + " can be found in <insert location>";
+
+        /* Call function to locate in the DB */
+        var dbItem = getLocationOfItem(itemSearchedFor);
+
+        if (dbItem && dbItem.Location) {
+            speechOutput = "Item " + itemSearchedFor + " is located in " + dbItem.Location + ". Goodbye.";
+            shouldEndSession = true;
+        } else {
+            speechOutput = "I cannot find item " + itemSearchedFor + " in the store. Please try again.";
+        }
+
+        // Need Confirmation? (Leave out for now..)
+        // speechOutput = "Are you looking for " + itemSearchedFor.value + "?";
+        // repromptText = "You can ask me where something is by saying, where is the salt?";
     } else {
-        speechOutput = "I'm not sure what you are looking for. Please try again";
-        repromptText = "I'm not sure what you are looking for. You can ask me where is the salt?";
+        speechOutput = "Sorry, I didn't catch what you are looking for. Please try again.";
+        repromptText = "Can I help you find something in the store?";
+        // TODO: Make suggestions to consumer? Read out available categories of items? (e.g. Meat, Dairy, Drinks, Frozen, ...)
     }
 
     callback(sessionAttributes,
-         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+         buildSpeechletResponse(intentName, speechOutput, repromptText, shouldEndSession));
 }
 
-function getLocationOfItem(intent, session, callback) {
-    // TODO:
-    var itemBeingSearched;
-    var repromptText = null;
-    var sessionAttributes = {};
-    var shouldEndSession = false;
-    var speechOutput = "";
+function getLocationOfItem(itemSearchedFor) {
+    // TODO: Fetch from DB !
+    // Consider having different location types (e.g. 'aisle 3', 'aisles 3-5', 'front of store', 'near the cashiers')
+    
+    // For now:
+    var db = [
+      {
+        "Item Name": "Apple",
+        "Location": "Aisle 1",
+        "Category": "Fruits"
+      },
+      {
+        "Item Name": "Apples",
+        "Location": "Aisle 1",
+        "Category": "Fruits"
+      },
+      {
+        "Item Name": "Orange",
+        "Location": "Aisle 1",
+        "Category": "Fruits"
+      },
+      {
+        "Item Name": "Bread",
+        "Location": "Aisle 2"
+      },
+      {
+        "Item Name": "Milk",
+        "Location": "Aisle 3",
+        "Category": "Dairy"
+      },
+      {
+        "Item Name": "Cheese",
+        "Location": "Aisle 3",
+        "Category": "Dairy"
+      },
+      {
+        "Item Name": "Eggs",
+        "Location": "Aisle 4"
+      },
+      {
+        "Item Name": "Apple Juice",
+        "Location": "Aisle 1"
+      },
+      {
+        "Item Name": "Beer",
+        "Location": "Aisle 7",
+        "Category": "Drinks"
+      },
+      {
+        "Item Name": "Coffee",
+        "Location": "Aisle 5",
+        "Category": "Drinks"
+      },
+      {
+        "Item Name": "Orange Juice",
+        "Location": "Aisle 1",
+        "Category": "Drinks"
+      },
+      {
+        "Item Name": "Pop Can",
+        "Location": "Aisle 5",
+        "Category": "Drinks"
+      },
+      {
+        "Item Name": "Salt",
+        "Location": "Aisle 6",
+        "Category": "Spices"
+      },
+      {
+        "Item Name": "Tea",
+        "Location": "Aisle 6",
+        "Category": "Drinks"
+      },
+      {
+        "Item Name": "Water",
+        "Location": "Aisle 1",
+        "Category": "Drinks"
+      },
+      {
+        "Item Name": "Pepper",
+        "Location": "Aisle 6",
+        "Category": "Spices"
+      },
+    }];
 
-    if (session.attributes) {
-        itemBeingSearched = session.attributes.itemBeingSearched;
+    for (var i=0; i< db.length; i++) {
+        if (db[i]["Item Name"] === itemSearchedFor) {
+            return db[i];
+        }
     }
-
-    if (itemBeingSearched) {
-        speechOutput = "Item " + itemBeingSearched + " is located in \'Aisle 3\'. Goodbye.";
-        shouldEndSession = true;
-    } else {
-        speechOutput = "I cannot find item " + itemBeingSearched + " in the store. Please try again.";
-    }
-
-    // Setting repromptText to null signifies that we do not want to reprompt the user.
-    // If the user does not respond or says something that is not understood, the session
-    // will end.
-    callback(sessionAttributes,
-         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+    return;
 }
-
 
 /**
  * Failed to recognize the item (confirmation answer is NO)
@@ -195,99 +269,6 @@ function failedToSetItemInSession(intent, session, callback) {
          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
 }
 
-
-
-/****** FROM COLOR PICKER SAMPLE... ****/
-// function createFavoriteColorAttributes(favoriteColor) {
-//     return {
-//         favoriteColor: favoriteColor
-//     };
-// }
-
-// function getColorFromSession(intent, session, callback) {
-//     var favoriteColor;
-//     var repromptText = null;
-//     var sessionAttributes = {};
-//     var shouldEndSession = false;
-//     var speechOutput = "";
-
-//     if (session.attributes) {
-//         favoriteColor = session.attributes.favoriteColor;
-//     }
-
-//     if (favoriteColor) {
-//         speechOutput = "Your favorite color is " + favoriteColor + ". Goodbye.";
-//         shouldEndSession = true;
-//     } else {
-//         speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-//             " is red";
-//     }
-
-//     // Setting repromptText to null signifies that we do not want to reprompt the user.
-//     // If the user does not respond or says something that is not understood, the session
-//     // will end.
-//     callback(sessionAttributes,
-//          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-// }
-
-// /**
-//  * Sets the color in the session and prepares the speech to reply to the user.
-//  */
-// function setColorInSession(intent, session, callback) {
-//     var cardTitle = intent.name;
-//     var favoriteColorSlot = intent.slots.Answer;
-//     var repromptText = "";
-//     var sessionAttributes = {};
-//     var shouldEndSession = false;
-//     var speechOutput = "";
-
-//     if (favoriteColorSlot) {
-//         var favoriteColor = favoriteColorSlot.value;
-//         sessionAttributes = createFavoriteColorAttributes(favoriteColor);
-//         speechOutput = "I now know your favorite color is " + favoriteColor + ". You can ask me " +
-//             "your favorite color by saying, what's my favorite color?";
-//         repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
-//     } else {
-//         speechOutput = "I'm not sure what your favorite color is. Please try again";
-//         repromptText = "I'm not sure what your favorite color is. You can tell me your " +
-//             "favorite color by saying, my favorite color is red";
-//     }
-
-//     callback(sessionAttributes,
-//          buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-// }
-
-// function createFavoriteColorAttributes(favoriteColor) {
-//     return {
-//         favoriteColor: favoriteColor
-//     };
-// }
-
-// function getColorFromSession(intent, session, callback) {
-//     var favoriteColor;
-//     var repromptText = null;
-//     var sessionAttributes = {};
-//     var shouldEndSession = false;
-//     var speechOutput = "";
-
-//     if (session.attributes) {
-//         favoriteColor = session.attributes.favoriteColor;
-//     }
-
-//     if (favoriteColor) {
-//         speechOutput = "Your favorite color is " + favoriteColor + ". Goodbye.";
-//         shouldEndSession = true;
-//     } else {
-//         speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-//             " is red";
-//     }
-
-//     // Setting repromptText to null signifies that we do not want to reprompt the user.
-//     // If the user does not respond or says something that is not understood, the session
-//     // will end.
-//     callback(sessionAttributes,
-//          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-// }
 
 // --------------- Helpers that build all of the responses -----------------------
 
